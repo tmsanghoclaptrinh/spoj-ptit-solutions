6d6bf512e5f17f4a4977e0ccce46d85ce4d04d36
tmsang <sangtran.d14ptit@gmail.com>
2023-09-16 04:26:47 +0700

    feat: Hoàn thành bài BCGCD - Ước chung lớn nhất, bội chung nhỏ nhất (Cơ bản)

---
+++ basic-problems/BCGCD.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+long long calculateGreatestCommonDivisor(long long firstNumber, long long secondNumber);
+long long calculateLeastCommonMultiple(long long firstNumber, long long secondNumber, long long greatestCommonDivisor);
+
+int main()
+{
+    while(1)
+    {
+        long long firstNumber, secondNumber;
+        scanf("%lld %lld", &firstNumber, &secondNumber);
+
+        if ((firstNumber == 0) && (secondNumber == 0))
+        {
+            break;
+        }
+            
+        long long greatestCommonDivisor = calculateGreatestCommonDivisor(firstNumber, secondNumber);
+        long long leastCommonMultiple = calculateLeastCommonMultiple(firstNumber, secondNumber, greatestCommonDivisor);
+        
+        printf("%lld %lld\n", greatestCommonDivisor, leastCommonMultiple);
+    }
+}
+
+long long calculateGreatestCommonDivisor(long long firstNumber, long long secondNumber)
+{
+    while (secondNumber != 0)
+    {
+        long long temp = secondNumber;
+        secondNumber = firstNumber % secondNumber;
+        firstNumber = temp;
+    }
+
+    return firstNumber;
+}
+
+long long calculateLeastCommonMultiple(long long firstNumber, long long secondNumber, long long greatestCommonDivisor)
+{
+    return firstNumber * secondNumber / greatestCommonDivisor; 
+}
