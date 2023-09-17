ae1c1918bc484c13b1e06f932d8ceeca844cb7e7
tmsang <sangtran.d14ptit@gmail.com>
2023-09-17 19:49:24 +0700

    feat: Hoàn thành bài "BCSTACK - Cấu trúc dữ liệu ngăn xếp (stack) (Cơ bản)" (Sử dụng container stack trong STL của C++)

---
+++ basic-problems/BCSTACK.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+stack<int> workingStack;
+
+int main()
+{
+    while (1)
+    {
+        string operation;
+        cin >> operation;
+        
+        if (operation == "end")
+        {
+            break;
+        }
+
+        if (operation == "init")
+        {
+            while (!workingStack.empty())
+            {
+                workingStack.pop();
+            }
+        }
+
+        if (operation == "push")
+        {
+            int n;
+            cin >> n;
+            workingStack.push(n);
+        }
+
+        if (operation == "pop")
+        {
+            if (!workingStack.empty())
+            {
+                workingStack.pop();
+            }
+        }
+
+        if (operation == "top")
+        {
+            if (workingStack.empty())
+            {
+                cout << "-1" << endl;
+            }
+            else
+                cout << workingStack.top() << endl;
+        }
+
+        if (operation == "size")
+        {
+            cout << workingStack.size() << endl;
+        }
+
+        if (operation == "empty")
+        {
+            if (workingStack.empty())
+            {
+                cout << "1" << endl;
+            }
+            else
+            {
+                cout << "0" << endl;
+            }
+        }
+    }
+}
