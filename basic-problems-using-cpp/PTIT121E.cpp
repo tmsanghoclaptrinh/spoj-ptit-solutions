0fb8f08176504cfedd3b56be2535f53937512862
Trần Minh Sáng <sangtran.d14ptit@gmail.com>
2024-01-19 14:18:33 +0700

    refactor: Sửa tên hàm, tên biến của bài Nguyên tố hóa học

--- basic-problems-using-cpp/PTIT121E.cpp
+++ basic-problems-using-cpp/PTIT121E.cpp
@@ -1,68 +1,111 @@
-# include <bits/stdc++.h>
-# define fi first
-# define se second
-# define all(a) a.begin() , e.end()
-# define ms(a , n) memset(a , n , sizeof(a))
-# define endl "\n"
+#include <iostream>
+#include <map>
+#include <stack>
+
 using namespace std;
 
-using ll = long long;
-using ld = long double;
-
-typedef pair <ll , ll> pi;
-typedef vector <pi> vii;
-
-const ll mod = 1e9 + 7;
-const ll base = 31;
-const ll maxn = 1e2 + 5;
-
-ll sum = 0 , n;
-string s;
-stack <ll> st;
-map <char , ll> mp;
+// Prototype của các hàm con
+stack<long long> convertFormulaToStack(string chemicalFormula);
+bool isAtom(char letter);
+long long calculateMolecularMass(stack<long long> chemicalStack);
 
 int main()
 {
-    ios_base :: sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    // Nhập vào công thức hóa học
+    string chemicalFormula;
+    cin >> chemicalFormula;
 
-    cin >> s;
-    n = s.size();
-    s = " " + s;
-    mp['O'] = 16 , mp['H'] = 1 , mp['C'] = 12;
-    for (ll i = 1 ; i <= n ; i++)
+    // Convert công thức hóa học sang stack
+    stack<long long> chemicalStack = convertFormulaToStack(chemicalFormula);
+
+    // Tính khối lượng của hóa chất dựa vào stack
+    long long molecularMass = calculateMolecularMass(chemicalStack);
+    cout << molecularMass;
+
+    return 0;
+}
+
+// Hàm convert công thức hóa học sang stack
+stack<long long> convertFormulaToStack(string chemicalFormula)
+{
+    stack<long long> chemicalStack;
+    map<char, long long> atomicMass;
+    atomicMass['H'] = 1;
+    atomicMass['C'] = 12;
+    atomicMass['O'] = 16;
+    chemicalFormula = " " + chemicalFormula; // Thêm khoảng trắng để xuống dưới vòng lặp bắt đầu chạy từ index = 1
+
+    for (long long i = 1; i <= chemicalFormula.size(); i++)
     {
-        if (s[i] == '(') st.push(-1);
-        if (s[i] == 'O' || s[i] == 'H' || s[i] == 'C') st.push(mp[s[i]]);
-        if (s[i] >= '1' && s[i] <= '9')
+        // Gặp dấu mở ngoặc đơn thì thêm số -1 vào đỉnh stack
+        if (chemicalFormula[i] == '(')
         {
-            if (!st.empty())
+            chemicalStack.push(-1);
+        }
+
+        // Gặp các ký tự 'H' hoặc 'C' hoặc 'O' thì thêm khối lượng của chúng vào đỉnh stack
+        if (isAtom(chemicalFormula[i]))
+        {
+            chemicalStack.push(atomicMass[chemicalFormula[i]]);
+        }
+
+        // Gặp các số từ 1 đến 9, thì lấy phần tử ở đỉnh stack ra để nhân với số này, sau đó push ngược lại vào đỉnh stack
+        if (chemicalFormula[i] >= '1' && chemicalFormula[i] <= '9')
+        {
+            if (!chemicalStack.empty())
             {
-                ll te = st.top(); st.pop();
-                te *= (s[i] - '0');
-                st.push(te);
+                long long temp = chemicalStack.top();
+                chemicalStack.pop();
+                temp *= (chemicalFormula[i] - '0');
+                chemicalStack.push(temp);
             }
         }
-        if (s[i] == ')')
+
+        // Gặp dấu đóng ngoặc đơn thì chạy vòng lặp để cộng dồn các số lưu trong stack, đến khi nào gặp số -1 (đại diện cho dấu ngoặc đơn)
+        if (chemicalFormula[i] == ')')
         {
-            ll te = 0;
-            while(!st.empty() && st.top() != -1)
+            long long temp = 0;
+
+            while (!chemicalStack.empty() && chemicalStack.top() != -1)
             {
-                ll top = st.top(); st.pop();
-                te += top;
+                long long top = chemicalStack.top();
+                chemicalStack.pop();
+                temp += top;
             }
-            if (!st.empty() && st.top() == -1)
+
+            // Kiểm tra nếu stack vẫn chưa rỗng, thì xóa bỏ số -1 (đại diện cho dấu ngoặc đơn) khỏi stack, rồi đẩy số temp đã tính được ở trên vào stack
+            if (!chemicalStack.empty() && chemicalStack.top() == -1)
             {
-                st.pop();
-                st.push(te);
+                chemicalStack.pop();
+                chemicalStack.push(temp);
             }
         }
     }
-    while(!st.empty())
+
+    return chemicalStack;
+}
+
+// Hàm kiểm tra ký tự truyền vào có phải là nguyên tố hóa học 'H' hoặc 'C' hoặc 'O' hay không
+bool isAtom(char letter)
+{
+    return letter == 'H' || letter == 'C' || letter == 'O';
+}
+
+// Hàm tính khối lượng của hóa chất dựa vào stack
+long long calculateMolecularMass(stack<long long> chemicalStack)
+{
+    long long molecularMass = 0;
+
+    // Duyệt qua các số còn lại trong stack để cộng dồn tính ra khối lượng của hóa chất ban đầu
+    while (!chemicalStack.empty())
     {
-        ll te = st.top(); st.pop();
-        if (te != -1) sum += te;
+        long long temp = chemicalStack.top();
+        chemicalStack.pop();
+        if (temp != -1)
+        {
+            molecularMass += temp;
+        }
     }
-    cout << sum;
+
+    return molecularMass;
 }
